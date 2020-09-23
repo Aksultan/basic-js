6db9fa8cb674fc71f389af143852f95360b50d51
Aksultan <aksultan919@gmail.com>
2020-09-24 03:14:22 +0600

    feat: add carbon-dating

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(twoDim) {
-  return [].concat.apply([], twoDim).filter(x=>x=='^^').length
+  return [].concat(...twoDim).filter(x=>x=='^^').length
 };
