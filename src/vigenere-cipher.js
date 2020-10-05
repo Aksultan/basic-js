3037a799bfdd47ea321eb7392c1cdfa737449e64
Aksultan <aksultan919@gmail.com>
2020-10-05 18:57:20 +0600

    feat: add vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reversed=true){
+    this.reversed = reversed
+  }
+  encrypt(str, key) {
+    key = key.toUpperCase()
+    key = key.padEnd(str.length, key)
+    let space = 0
+    let answer = str.toUpperCase().split("").map((x,index)=>x.match(/\W|_|[0-9]/g)?((x==" "?(space++,x):x)):(String.fromCharCode((x.charCodeAt()+key[index-space].charCodeAt())%26+65)))
+	  return this.reversed?answer.join(""):answer.reverse().join("")
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    key = key.toUpperCase()
+    key = key.padEnd(str.length, key)
+    let space = 0
+    let answer = str.toUpperCase().split("").map((x,index)=>x.match(/\W|_|[0-9]/g)?((x==" "?(space++,x):x)):(String.fromCharCode((x.charCodeAt()-key[index-space].charCodeAt()+26)%26+65)))
+	  return this.reversed?answer.join(""):answer.reverse().join("")
   }
 }
+
 
 module.exports = VigenereCipheringMachine;
